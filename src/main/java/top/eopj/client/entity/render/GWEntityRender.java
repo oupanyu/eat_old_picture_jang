81a74ad9241001a7119da65e5e00bd87946e9f83
oupanyu <oupanyu615@hotmail.com>
2023-10-16 13:35:43 +0800

    first

---
+++ src/main/java/top/eopj/client/entity/render/GWEntityRender.java
@@ -0,0 +1,20 @@
+package top.eopj.client.entity.render;
+
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.util.Identifier;
+import top.eopj.EatOldPictireJiang;
+import top.eopj.client.entity.model.HumanEntityModel;
+import top.eopj.common.entity.GameWaterEntity;
+
+public class GWEntityRender extends MobEntityRenderer<GameWaterEntity, HumanEntityModel<GameWaterEntity>> {
+    public GWEntityRender(EntityRendererFactory.Context context) {
+        super(context,new HumanEntityModel<>(context.getPart(EntityModelLayers.PLAYER)), 0.5f);
+    }
+
+    @Override
+    public Identifier getTexture(GameWaterEntity entity) {
+        return new Identifier(EatOldPictireJiang.MODID, "textures/human/game_water.png");
+    }
+}
---
+++ src/main/java/top/eopj/common/entity/GameWaterEntity.java
@@ -0,0 +1,33 @@
+package top.eopj.common.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+public class GameWaterEntity extends AbstractModHumanEntity{
+    public GameWaterEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+        super(entityType, world);
+    }
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0f));
+        this.goalSelector.add(8, new LookAroundGoal(this));
+        this.goalSelector.add(2, new AttackGoal(this));
+        this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0));
+    }
+
+    @Override
+    public boolean tryAttack(Entity target) {
+        boolean bl = super.tryAttack(target);
+        if (bl) {
+            float f = this.getWorld().getLocalDifficulty(this.getBlockPos()).getLocalDifficulty();
+            if (this.getMainHandStack().isEmpty() && this.isOnFire() && this.random.nextFloat() < f * 0.3f) {
+                target.setOnFireFor(2 * (int)f);
+            }
+        }
+        return bl;
+    }
+}
